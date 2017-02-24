aa90d2eaaf9bee01020487e0260b724889b4a8e6
yeongtaik byeon <yt.byeon@samsung.com>
2017-02-24 17:00:26 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,84 @@
+function checkSNEE() {
+    var x = undefined + 0;
+    if ( x == NaN) {
+        x = 0;
+    }
+}
+
+function checkIAL() {
+    var length = 0;
+    length = 2;
+    array = new Array(length) ;
+}
+
+
+function checkSNAS() {
+
+    var numArray = [11, 3, 2];
+    //numArray.sort();
+}
+
+
+function checkSTCP() {
+
+    if ( typeof xx == 'undefind' ) {
+
+        x = {};
+    }
+}
+
+
+function checkIPD() {
+
+    var a = {};
+
+    /*Object.defineProperties(a, 'x', {
+        value: 42,
+        get: function () {
+            return 42 ;
+        }
+    });*/
+}
+
+function checkSNEE1() {
+
+    var x = undefined + 0;
+    if ( x == NaN) {
+        x = 0;
+    }
+}
+
+function checkIAL1() {
+    var length = 0;
+    length = 2;
+    array = new Array(length) ;
+}
+
+
+function checkSNAS1() {
+
+    var numArray = [11, 3, 2];
+    numArray.sort();
+}
+
+
+function checkSTCP1() {
+
+    if ( typeof xx == 'undefind' ) {
+
+        x = {};
+    }
+}
+
+
+function checkIPD1() {
+
+    var a = {};
+
+    Object.defineProperties(a, 'x', {
+        value: 42,
+        get: function () {
+            return 42 ;
+        }
+    });
+}
