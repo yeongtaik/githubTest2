6f3d1084f3daed145f41d562ce7e444ca700b3fc
yeongtaik byeon <yt.byeon@samsung.com>
2017-11-23 14:22:38 +0900

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,5 +1,16 @@
 // This will show examples related with the Common Weakness Enumeration (CWE).
-
+function CWE_398() { // IDENTICAL_BRANCHES
+    if (x >= 0) {
+        y = x;
+    } else {
+        y = x;
+    }
+}
+function CWE_476() { // NULL_POINTER
+    var obj;
+    var y = obj.x;
+    console.log(y);
+}
 
 
 function CWE_480() { // BAD_BITWISE_OPERATOR
